323cdf059a883d27b4c857b7d86782500356aa34
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2014-02-27 23:54:26 +0100

    update step pages + add step 3

---
+++ NG-Wakanda-Pack/WebFolder/scripts/step3.js
@@ -0,0 +1,14 @@
+﻿angular.module('step2', ['wakConnectorModule']);
+
+function Controller($scope, wakConnectorService) {
+
+    // Create a proxy of the server model
+    wakConnectorService.init().then(function (ds) {
+ 
+        // once ready use the datastore on the $scope
+        // feed the angular scope 
+        // with the stored data of the Country DataClass
+        $scope.countries = ds.Country.$find({});
+    });
+
+}
