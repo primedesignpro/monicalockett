4017c7ab77d5a055a11dada7463b76c4c6d09914
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2014-02-28 17:32:06 +0100

    start page 5

---
+++ NG-Wakanda-Pack/WebFolder/scripts/step5.js
@@ -0,0 +1,1 @@
+﻿angular.module('step5', ['wakConnectorModule']);function Controller($scope, wakConnectorService) {    // Create a proxy of the server model    wakConnectorService.init('Employee').then(function (ds) {         ds.Employee.$find({}).then(function (event) {            $scope.employees = event.result;        });    });}
