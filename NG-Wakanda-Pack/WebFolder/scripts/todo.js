beb7181875e6221954840b5199dfeda92f35cd4b
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2014-04-22 11:35:06 +0200

    fix "todo.js " indentation - remove global var

--- NG-Wakanda-Pack/WebFolder/scripts/todo.js
+++ NG-Wakanda-Pack/WebFolder/scripts/todo.js
@@ -1,36 +1,34 @@
 'use strict';
 
-var Item;
-
 angular.module('todo', ['wakConnectorModule']);
 
 function Todo($scope, wakConnectorService) {
   wakConnectorService.init('Item').then(function (ds) {
 
-  $scope.items = ds.Item.$find({});
-    
-  $scope.add = function() {
-    var item = ds.Item.$create({text: $scope.newText, done: false});
-    $scope.items.push(item);
-    $scope.newText = '';
-    item.$save();
-  };
+      $scope.items = ds.Item.$find({});
+        
+      $scope.add = function() {
+        var item = ds.Item.$create({text: $scope.newText, done: false});
+        $scope.items.push(item);
+        $scope.newText = '';
+        item.$save();
+      };
 
-  $scope.remaining = function() {
-    return $scope.items.reduce(function(count, item) {
-      return item.done ? count : count + 1;
-    }, 0);
-  };
+      $scope.remaining = function() {
+        return $scope.items.reduce(function(count, item) {
+          return item.done ? count : count + 1;
+        }, 0);
+      };
 
-  $scope.archive = function() {
-    $scope.items = $scope.items.filter(function(item) {
-      if (item.done) {
-        item.$remove();
-        return false;
-      }
-      return true;
-    });
-  };
+      $scope.archive = function() {
+        $scope.items = $scope.items.filter(function(item) {
+          if (item.done) {
+            item.$remove();
+            return false;
+          }
+          return true;
+        });
+      };
 
   });
 }
