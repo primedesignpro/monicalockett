f8334caed9c910388a8b07a3b708beb7b1ee3536
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2014-07-01 14:13:15 +0200

    few fixes and more infinite scroll

--- NG-Wakanda-Pack/WebFolder/demos/bootstrap/app.js
+++ NG-Wakanda-Pack/WebFolder/demos/bootstrap/app.js
@@ -1,6 +1,19 @@
 'use strict';
 
-angular.module('playAngular', ['wakanda']);
+angular.module('playAngular', ['wakanda'])
+
+.directive('whenScrolled', function() {
+    return function(scope, elm, attr) {
+        var raw = elm[0];
+        
+        elm.bind('scroll', function() {
+            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+                scope.$apply(attr.whenScrolled);
+            }
+        });
+    };
+});
+
 
 
 function PlayController($scope, $wakanda) {
