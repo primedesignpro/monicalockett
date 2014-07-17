3dc9350eb1d328bf28a3b58d7c954c135b24e741
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2014-07-18 00:48:12 +0200

    init update

--- NG-Wakanda-Pack/Workers/initApp-sharedWorker.js
+++ NG-Wakanda-Pack/Workers/initApp-sharedWorker.js
@@ -1,5 +1,5 @@
 const
-    COUNT_EMPLOYEES_TO_CREATE = 20000,
+    COUNT_EMPLOYEES_TO_CREATE = 100000,
     ADMIN_PASSWORD = 'admin';
 
 var
@@ -12,15 +12,20 @@ self.onconnect = function initAppWorkerOnConnect(event) {
 
     ds.setCacheSize(1024 * 1024 * 1024); // 1 Gb
 
+
+    /**************************************************/
+    /*   INIT THE COUNTRY / COMPANY / EMPLOYEE DATA   */
+    /**************************************************/
+
     // if data empty, generate fake data
-    if (ds.Employee.length === 0) {
+    //if (ds.Employee.length === 0) {
     	generator = require("fakedata/generator");
     	loginByPassword('admin', ADMIN_PASSWORD);
     	console.info(" ::::: Creating ", COUNT_EMPLOYEES_TO_CREATE, " Employees  Companies...");
-    	generator.buildFakeData(COUNT_EMPLOYEES_TO_CREATE, {log: false});
+    	generator.buildFakeData(COUNT_EMPLOYEES_TO_CREATE, {log: false, remove: true});
     	console.info(" End creating Employees. Employees: ", ds.Employee.length, ", Companies: ", ds.Company.length, ", Country: ", ds.Country.length, " ::::: ");
     	ds.flushCache();
-    }
+    //}
 
     // Warm the cache with a sequential query that will load all logs
     console.log('Warming the cache..........');
@@ -34,12 +39,51 @@ self.onconnect = function initAppWorkerOnConnect(event) {
     }
     console.log('Warming the cache..........done');
 
-    if (storage.PRODUCTION_MODE) {
-    	privateSettingsFile = File(getFolder().path + 'PRIVATE-SETTING-nogit.js'); 
-    	if (privateSettingsFile.exists) {
-    		addHttpRequestHandler('/getRequestCount', 'HttpRequestHandlers/requestCount.js', 'getRequestCount');
-    	}
-    }
+
+    /**************************************************/
+    /*            RE-INIT THE TODO DATA               */
+    /**************************************************/
+
+    ds.Item.remove();
+    [
+        {ID: 1, text: 'Try AngularJS', done: true},
+        {ID: 2, text: 'Try Angular-Wakanda', done: false},
+        {ID: 3, text: 'Play with Wakanda', done: false},
+        {ID: 4, text: 'Have Fun!', done: false}
+    ].forEach(function addTodo(todo) {
+        var item = new ds.Item(todo);
+        item.save();
+    });
+
+    /**************************************************/
+    /*       RE-INIT THE GROUP / CONTACT DATA         */
+    /**************************************************/
+
+
+    var groups;
+    ds.Group.remove();
+    ds.Contact.remove();
+    [
+        {ID: 1, name: 'Coworkers'},
+        {ID: 2, name: 'Friends'},
+        {ID: 3, name: 'Family'},
+    ].forEach(function addGroup(group) {
+        group = new ds.Group(group);
+        group.save();
+        groups[group.name] = group;
+    });
+    [
+        {name: 'Cindy',   group: groups.Coworkers, mobile: '+1 234567890'},
+        {name: 'Jerry',   group: groups.Coworkers, mobile: '+1 345678901'},
+        {name: 'Paul',    group: groups.Friends,   mobile: '+1 456789012'},
+        {name: 'Bill',    group: groups.Friends,   mobile: '+1 567890123'},
+        {name: 'Hilary',  group: groups.Friends,   mobile: '+1 678901234'},
+        {name: 'Cindy',   group: groups.Family,    mobile: '+1 789012345'},
+        {name: 'Phillip', group: groups.Family,    mobile: '+1 890123456'}
+    ].forEach(function addContact(contact) {
+        contact = new ds.Group(contact);
+        contact.save();
+    });
 
     console.log('close the initApp shared worker');
     self.close();
