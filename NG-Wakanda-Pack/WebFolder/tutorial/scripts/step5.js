92531447c39e6088f5a652d05e1ba29c08a48c37
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2014-10-17 15:50:26 +0200

    remove obsolete hack

--- NG-Wakanda-Pack/WebFolder/tutorial/scripts/step5.js
+++ NG-Wakanda-Pack/WebFolder/tutorial/scripts/step5.js
@@ -4,29 +4,17 @@ function Controller($scope, $wakanda) {
 
     // Create a proxy of the server model
     $wakanda.init().then(function oninit(ds) {
-
+ 
         // feed the angular scope with the stored data of the Country DataClass
         $scope.countries = ds.Country.$find();
-        
-        // manage when a country is choosen
-        $scope.$watch('country', function fetchRelatedCompanies(country) {
-            if (!country) return;
-            // get companies related to current country
-            country.companies.$fetch();
 
-        });
+    });
 
-        
-        // hack to get alias attribute available
-        $scope.$watch('company', function fetchShowCompany(company) {
-            if (!company) return;
-            if (company.managerName) return;
-            console.warn('the alias "managerName" attribute is missing:', company);
-            console.log('manually add the managerName value from internal properties');
-            company.managerName = company.$_entity.managerName.value;
-        });
-        
-
+    // manage when a country is choosen
+    $scope.$watch('country', function fetchRelatedCompanies(country) {
+        if (!country) return;
+        // get companies related to current country
+        country.companies.$fetch();
     });
 
 }
